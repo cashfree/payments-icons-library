867072f499c9ab0b5a5083d88ec9d754f3d493d3
Bhaskar Aggarwal <bhaskaraggarwal@CFs-MacBook-Pro-23.local>
2022-09-06 00:56:06 +0530

    Updated name

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
         filename: "image-library.js",
         globalObject: "this",
         library: {
-            name: "financeIcons",
+            name: "paymentsIcons",
             type: "umd",
         },
     },
